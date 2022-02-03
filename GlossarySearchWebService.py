59ea187a3608f27057ec83286ab68c3f42bf5210
Philippe Krajsic <philippekrajsic@gmail.com>
2022-02-03 10:23:53 +0100

    Update GlossarySearchWebService.py

--- GlossarySearchWebService.py
+++ GlossarySearchWebService.py
@@ -72,7 +72,7 @@ def getLinksForTokens(tokens, name_links):
 
 def check_message_validity(message):
     isvalid = False
-    print(message['metaData']['intent']['output'])
+    print(message['metaData'])
     output = message['metaData']['intent']['output']
     for msg in output:
         if (msg["type"] == 'message' and msg['data'] and msg['data']['type'] == 'text/plain'):
